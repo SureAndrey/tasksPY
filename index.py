ca9033fe214fced70aba241385204a739ce386d7
Andrey Duarte <sureandrey@gmail.com>
2023-01-22 14:29:55 -0300

    feat: funções basicas - adicionar, remover, marcar

--- index.py
+++ index.py
@@ -1,11 +1,38 @@
 import json
+import re
 
+# selecionar o json
 with open('tasks.json') as file:
-    tasks = json.load(file) 
-    tasks['tasks'][0]['nao-feitas'] = "n"
+    tasks = json.load(file)
 
-print(tasks["tasks"][0]["nao-feitas"])
+nao_feitas = tasks['tasks'][0]['nao-feitas']
+feitas = tasks['tasks'][0]['feitas']
 
-with open('tasks.json', 'w') as file:
-    json.dump(tasks, file)
+print(f'Não feitas: {nao_feitas}\n')
+print(f'Feitas: {feitas}')
 
+while True:
+    comando = input("> ")
+
+    # adicionar item
+    if re.findall('^add', comando) == ['add']:
+        comando = re.sub('^add ', '', comando)
+        nao_feitas.append(comando)
+    # remover item especifico
+    elif re.findall('^rm', comando) == ['rm']:
+        comando = re.sub('^rm ', '', comando)
+        nao_feitas.pop(int(comando))
+    # comando para remover todos não feitos
+    elif re.findall('^rall', comando) == ['rall']:
+        nao_feitas.clear()
+    elif re.findall('^mark', comando) == ['mark']:
+        comando = re.sub('^mark ', '', comando)
+        feitas.append(nao_feitas[int(comando)])
+        nao_feitas.pop(int(comando))
+
+    print(f'Não feitas: {nao_feitas}\n')
+    print(f'Feitas: {feitas}')
+
+    # escrever no json
+    with open('tasks.json', 'w') as file:
+        json.dump(tasks, file)
