b9054f34fd4fcf45f82089f21c239b7d83a7cd4b
Andrey Duarte <sureandrey@gmail.com>
2023-01-22 13:15:26 -0300

    feat: acessando o json pelo python

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
 import json
+
+with open('tasks.json') as file:
+    tasks = json.load(file) 
+    tasks['tasks'][0]['nao-feitas'] = "n"
+
+print(tasks["tasks"][0]["nao-feitas"])
+
+with open('tasks.json', 'w') as file:
+    json.dump(tasks, file)
+
